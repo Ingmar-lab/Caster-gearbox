2703e36989f7166990bcc3024e949bce3e43d752
Ingmar Osterberg <ingmar.osterberg@gmail.com>
2024-09-17 13:05:53 +0300

    Adding bad tc

--- student_AutomaticGearbox.py
+++ student_AutomaticGearbox.py
@@ -14,25 +14,42 @@ def student_AutomaticGearbox(gear, RPM, longAcc, velocity, throttle, distance, t
     wheelCircumference = 2 * wheel_radius * math.pi
     speed = velocity * 3.6
     gear_demand = gear
+    
     currentGearRatio = gear_ratios[gear_demand - 1]
-    currentWheelSpeed = RPM /60 * 1/currentGearRatio * 1/final_drive_ratio * wheelCircumference
+    currentWheelSpeed = RPM /60 * 1/currentGearRatio * 1/final_drive_ratio * wheelCircumference * 3.6
+    optimalSlip_ = optimal_slip / 100 
+    wheelSlip = 1
+    if speed != 0:
+        wheelSlip = (currentWheelSpeed / speed) - 1
+    else:
+        wheelSlip = 0
     if gear_demand == 0:
         gear_demand = 1
     if RPM > 9000 and gear != 4:
-        gear_demand = gear_demand + 1
+       gear_demand = gear_demand + 1
+       
+    if speed == 0:
+        throttle = 0.01
+    elif(wheelSlip > optimalSlip_):
+        throttle = throttle * 0.999
+    elif(wheelSlip < optimalSlip_):
+        throttle = throttle * 1.10
         
-    if gear == 1:
-        throttle = 0.3
-    elif gear == 2:
-        throttle = 0.5
-    elif gear == 3:
-        throttle = 0.8
-    else:
-        throttle = 1
-    print(currentWheelSpeed , speed / 3.6)
+    if throttle > 1:
+        throttle = 1  
+    
+    
+    # if gear == 1:
+    #     throttle = 0.3
+    # elif gear == 2:
+    #     throttle = 0.5
+    # elif gear == 3:
+    #     throttle = 0.8
+    # else:
+    #     throttle = 1
+    print(throttle, wheelSlip, optimalSlip_, timeLap, gear, longAcc)
     #print(wheelCircumference)
     # For comparison, uncomment the line below to see how Caster gearbox performs
     #gear_demand = caster_AutomaticGearbox(gear, RPM, longAcc, velocity, throttle, distance, timeLap)
-
     #Return the selected gear
     return (gear_demand, throttle)
