32e195cbc78e50fb6c4c8e3243444b55396628a6
Ingmar-lab <ingmar.osterberg@gmail.com>
2024-09-25 17:12:12 +0200

    Update student_AutomaticGearbox.py

--- student_AutomaticGearbox.py
+++ student_AutomaticGearbox.py
@@ -1,3 +1,9 @@
+#Programhuvud för automatisk växellåda för bilsimulator
+#Ingmar Österberg och Arvid Nilsson
+#Datum: 25/09/24
+#Beskriving: Detta program läser in data från en simulerad bil och ger optimal växling baserat på varvtalen
+#samt modullerar gas baserat på hjulspinn.
+
 from caster_AutomaticGearbox import caster_AutomaticGearbox
 from Car import Car
 import math
@@ -19,13 +25,13 @@ def student_AutomaticGearbox(gear, RPM, longAcc, velocity, throttle, distance, t
     # Converting speed to km/h
     speed = velocity * 3.6
 
-    # calculating the wheel circumferance 
+    # Calculating the wheel circumferance 
     wheelCircumference = 2 * wheel_radius * math.pi
     
     #Getting the current gear ratio from the transmission
     currentGearRatio = gear_ratios[gear_demand - 1]
 
-    # Deffining the initial throttle in each gear
+    # Defining the initial throttle in each gear
     beginningThrottleByGear = [0.5,0.8,1,1]
 
     # Calculating the current wheel slip and wheel speed
@@ -34,22 +40,22 @@ def student_AutomaticGearbox(gear, RPM, longAcc, velocity, throttle, distance, t
     if speed != 0:
         wheelSlip = (currentWheelSpeed / speed) - 1
 
-        #Making shure the wheel speed is positive
+        #Making sure the wheel speed is positive
         if wheelSlip < 0:
             wheelSlip = 0
     else:
         wheelSlip = optimalSlip_
         throttle = beginningThrottleByGear[0]
 
-    # Making shure that the car does not start in neutral
+    # Making sure that the car does not start in neutral
     if gear_demand == 0:
         gear_demand = 1
 
-    # Shifting if rpm if over 9000 and the wheels are not slipping to much
+    # Shifting if rpm is over 9000 and the wheels are not slipping too much
     if RPM > 9000 and gear != 4 and wheelSlip <= 1.3:
         gear_demand = gear_demand + 1
 
-        #Setting the initial throtle in each gear
+        #Setting the initial throttle in each gear
         throttle = beginningThrottleByGear[gear_demand - 1]
 
     #Adding throttle if car is not slipping enough
@@ -58,7 +64,7 @@ def student_AutomaticGearbox(gear, RPM, longAcc, velocity, throttle, distance, t
     elif wheelSlip < optimalSlip_:
         throttle += 0.01 * gear_demand
         
-    #Making shure that throttle does not surpas 100% = 1
+    #Making sure that throttle does not surpas 100% = 1
     if throttle > 1:
         throttle = 1 
     
